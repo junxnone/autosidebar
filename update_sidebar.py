e54b9c049261e984c145d8eae8341a0cd127d02e
JunX <junchen0102@gmail.com>
2022-12-26 20:24:18 +0800

    Update update_sidebar.py

--- update_sidebar.py
+++ update_sidebar.py
@@ -4,64 +4,22 @@ import pandas as pd
 import argparse
 import fileinput
 
+
 ap = argparse.ArgumentParser()
-ap.add_argument('--path', default='none')
+ap.add_argument('-p', '--path', default='none')
 args = ap.parse_args()
 
-doc_ptn = os.path.join(args.path, '*.md')
-file_list = glob.glob(doc_ptn)
 
-def txt2list(file_path):
-    wlist = []
-    with open(file_path, 'r') as fn:
-        flines = fn.readlines()
-        for line in flines:
-            temp = line.strip('\n')
-            wlist.append(temp)
-    return wlist
+def search_docs(docs_path):
+    doc_ptn = os.path.join(docs_path, '*.md')
+    files_list = glob.glob(doc_ptn)
 
-sort_fpath = os.path.join(args.path, 'sidebar.md')
-sort_list = []
-if sort_fpath in file_list:
-    sort_list = txt2list(sort_fpath)
-
-ignore_files = ['_sidebar.md', 'NAV.md', 'README.md', 'sidebar.md', 'hist.md']
-for igf in ignore_files:
-    igf_path = os.path.join(args.path, igf)
-    if igf_path in file_list:
-        file_list.remove(igf_path)
-
-fd = {"filepath": pd.Series(file_list)}
-fdf = pd.DataFrame(fd)
-fdf.filepath.str.split("/", expand=True)
-fdf["filename"] = fdf.filepath.str.split("/", expand=True)[1]
-namex = fdf.filename.str.split(".", expand=True)[0].rename("name0")
-fdf = fdf.join(namex)
-
-cls_df = fdf.filename.str.replace('.', '_', regex=False).str.split("_", expand=True, regex=False)
-fdf = fdf.join(cls_df)
-fdf.fillna(value="None", inplace=True)
-loop_max = fdf.columns.tolist()[-1]
-fdf[fdf.columns.tolist()[-1] + 1] = "None"
-
-def search_sort_cls(df, level):
-    cls_list = df[level].value_counts().index.tolist()
-    if "None" in cls_list:
-        cls_list.remove("None")
-    if "" in cls_list:
-        cls_list.remove("")
-    return cls_list
-
-
-def build_subname(a,b):
-    if b is None:
-        return a
-    else:
-        return b
-
-sname = fdf.name0.str.rsplit('_', expand=True, n=1)
-sname['lastname'] = sname.apply(lambda x: build_subname(x[0], x[1]), axis = 1)
-fdf['lastname'] = sname['lastname']
+    ignore_files = ['_sidebar.md', 'NAV.md', 'README.md', 'sidebar.md', 'hist.md']
+    for igf in ignore_files:
+        igf_path = os.path.join(docs_path, igf)
+        if igf_path in files_list:
+            files_list.remove(igf_path)
+    return files_list
 
 def get_title(filepath, lastname):
     title = 'notitle'
@@ -74,56 +32,153 @@ def get_title(filepath, lastname):
         return lastname
     return title
 
-fdf['title'] = fdf.apply(lambda x: get_title(x['filepath'], x['lastname']), axis = 1)
+def build_subname(a,b):
+    if b is None:
+        return a
+    else:
+        return b
 
 def get_create_date(filepath):
     date = 'nodate'
     for line in fileinput.input(files=filepath):
         if line.startswith('Created @'):
-            date = line.replace('Created @ | `', '').replace('Z`', '').strip('\n').strip(' ')
+            date = line.replace('Created @ | `', '').split('T')[0]
             break
     fileinput.close()
 
     return date
 
-fdf['create_date'] = fdf.apply(lambda x: get_create_date(x['filepath']), axis = 1)
-fdf['link'] = '[' + fdf.title + '](/' + fdf.name0 + ')'
 
-date_linkl = fdf.sort_values('create_date', ascending=False).link.tolist()
-date_datel = fdf.sort_values('create_date', ascending=False).create_date.tolist()
+
+def build_df(files_list):
+    files_dict = {"filepath": pd.Series(files_list)}
+    files_df = pd.DataFrame(files_dict)
+    files_df["filename"] = files_df.filepath.str.split("/", expand=True)[1]
+
+    basename = files_df.filename.str.split(".", expand=True)[0].rename("basename")
+    files_df = files_df.join(basename)
+    xpath = files_df.basename.str.replace("[0-9][0-9][0-9][0-9]_", '', regex=True).str.replace('_','/').rename("xpath")
+    files_df = files_df.join(xpath)
+
+
+
+    sname = files_df.basename.str.rsplit('_', expand=True, n=1)
+    sname['lastname'] = sname.apply(lambda x: build_subname(x[0], x[1]), axis = 1)
+    files_df['lastname'] = sname['lastname']
+    files_df['title'] = files_df.apply(lambda x: get_title(x['filepath'], x['lastname']), axis = 1)
+    files_df['create_date'] = files_df.apply(lambda x: get_create_date(x['filepath']), axis = 1)
+    cls_df = files_df.filename.str.replace("[0-9][0-9][0-9][0-9]_", '', regex=True).str.replace('.', '_', regex=False).str.split("_", expand=True, regex=False)
+    files_df = files_df.join(cls_df)
+    files_df.fillna(value="None", inplace=True)
+    files_df['link'] = '[' + files_df.title + '](/' + files_df.basename + ')'
+
+    return files_df
+
+def build_rules(file_path):
+    listpath = []
+    lstk = []
+    lastspc = 0
+    with open(file_path, 'r') as fn:
+        flines = fn.readlines()
+        for line in flines:
+            temp = line.strip('\n')
+            if temp.startswith(' '):
+                ltitle = temp.replace(' ', '').replace('-', '')
+                spc = temp.count('  ')
+                if lastspc < spc:
+                    lstk.append(ltitle)
+                elif lastspc == spc:
+                    lstk.pop()
+                    lstk.append(ltitle)
+                else:
+                    lstk=lstk[:-2]
+                    lstk.append(ltitle)
+                lastspc = spc
+            if temp.startswith('- '):
+                ltitle = temp.replace('- ', '')
+                lstk = [ltitle]
+                lastspc = 0
+            listpath.append('/'.join(lstk))
+    return listpath
+
+spath = 'sidebar.md'
+rulespath = build_rules(spath)
+flist = search_docs(args.path)
+fdf = build_df(flist)
+
+def series2set(ses):
+    cls_set = ses.value_counts().index.tolist()
+    if "None" in cls_set:
+        cls_set.remove("None")
+    if "" in cls_set:
+        cls_set.remove("")
+    if "md" in cls_set:
+        cls_set.remove("md")
+    return cls_set
+
+def buildsortseq(rulespath):
+    sdf = pd.DataFrame(rulespath, columns=['path'])
+    sdf = sdf.path.str.split('/', expand=True)
+    sdf.fillna(value="None", inplace=True)
+
+    return sdf
+
+def resort_list(inlist, seqlist):
+    rlist = []
+    for it in seqlist:
+        if it in inlist:
+            rlist.append(it)
+    olist = list(set(inlist) - set(rlist)) 
+    return rlist + sorted(olist)
+
+from functools import reduce
+
+
+rdup = lambda x,y:x if y in x else x + [y]
+
+
+def sort_df(df, index, seq_df):
+    curlist = series2set(df[index])
+    seqlist = []
+    if index < seq_df.columns.size:
+        seqlist = seq_df[index].tolist()
+        seqlist = reduce(rdup, [[], ] + seqlist)
+        if 'None' in seqlist:
+            seqlist.remove("None")
+    curlist = resort_list(curlist, seqlist)
+    strhead = '  ' * index + '- '
+    for it in curlist:
+        sub_df = df[df[index] == it].reset_index(drop=True)
+        itdf = sub_df[(sub_df[index+1] == 'md') & (sub_df[index] == it)]
+        subseq_df = pd.DataFrame()
+        if index < seq_df.columns.size:
+            subseq_df = seq_df[seq_df[index] == it].reset_index(drop=True)
+        if len(itdf) > 0:
+            for il in itdf.link:
+                print(strhead + il)
+        else:
+            print(strhead + it)
+        if len(sub_df) > 0:
+            sort_df(sub_df, index + 1, subseq_df)
+ 
+seq_df = buildsortseq(rulespath)
+
+sort_df(fdf, 0, seq_df)
+
+
+datedf = fdf.sort_values('create_date', ascending=False)
+datedf['flink'] = '[' + fdf.xpath.str.replace('/', ' ') + '](/' + fdf.basename + ')'
+
+date_linkl = datedf.flink.tolist()
+date_datel = datedf.create_date.tolist()
 
 hist_list = ['# Wiki History\n']
-
 for ilink, idate in zip(date_linkl, date_datel):
     if idate != 'nodate':
-        ds = idate.split('T')[0]
-        hist_list.append(f'- {ds}   {ilink}')
+        hist_list.append(f'- {idate}' + ' '*8 +  ilink)
 
 def write_list2txt(wlist, file_path):
     with open(file_path, 'w') as fn:
         for line in wlist:
             fn.write(str(line) + '\n')
 write_list2txt(hist_list, 'docs/hist.md')
-
-def loop_cls(ldf, sloop, nloop, maxloop):
-    list_0 = search_sort_cls(ldf, nloop)
-    list_0s = []
-    for ix in sort_list:
-        ix = ix.replace(' ', '')
-        if ix in list_0:
-            list_0s.append(ix)
-            list_0.remove(ix)
-    list_x = list_0s + list_0
-    for cls0n in list_x:
-        cls0_df = ldf[ldf[nloop] == cls0n]
-
-        if (len(cls0_df[cls0_df[nloop+1] == 'md'])):
-            for ll in cls0_df[cls0_df[nloop+1] == 'md'].link:
-                print((' ' * (nloop - sloop) * 2) + (f'- {ll}'))
-        elif (cls0n != 'md'):
-            print((' ' * (nloop - sloop) * 2) + f'- {cls0n}')
-        if nloop < maxloop:
-            loop_cls(cls0_df, sloop, nloop+1, maxloop)
-
-loop_cls(fdf, 1, 1, 5)
-print('- [Wiki History](/hist)')
